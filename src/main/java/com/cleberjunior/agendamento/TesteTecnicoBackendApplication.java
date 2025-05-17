78f65398545e3fe20664da4aebbe231d403a3ae9
Cleber Farias <jrf.bynho@hotmail.com>
2025-05-17 20:13:54 -0300

    initial commit

---
+++ src/main/java/com/cleberjunior/agendamento/TesteTecnicoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cleberjunior.agendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteTecnicoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteTecnicoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cleberjunior/agendamento/TesteTecnicoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleberjunior.agendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteTecnicoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
